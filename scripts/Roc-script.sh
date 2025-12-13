474a1be1b68db9e59673ed25c8a2dbda102dec15
老王 <84431421+wjt12300@users.noreply.github.com>
2025-12-13 22:40:40 +0800

    更新 Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -45,8 +45,6 @@ git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome 
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora feeds/luci/themes/luci-theme-aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config feeds/luci/applications/luci-app-aurora-config
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
